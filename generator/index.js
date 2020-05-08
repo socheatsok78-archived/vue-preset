5e0a02d57e0ff2754ce9c437f924b23453288ef7
Socheat Sok <alex@socheat.net>
2020-05-08 12:10:07 +0700

    update github action for e2e and unit test

--- generator/index.js
+++ generator/index.js
@@ -5,6 +5,8 @@ const ApiPlugin = require('./app/app.plugin')
 const i18nPlugin = require('./i18n/i18n.plugin')
 const MockPlugin = require('./mocks/mocks.plugin')
 
+const GitHubPlugin = require('./github/github.plugin')
+
 module.exports = (api, options, rootOptions) => {
   // Install additional dependencies
   api.extendPackage(packages)
@@ -20,4 +22,7 @@ module.exports = (api, options, rootOptions) => {
 
   // Inject mocks to root
   MockPlugin.install(api)
+
+  // Install GitHub Actions
+  GitHubPlugin.install(api)
 }
